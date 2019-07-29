50cb5fc379ba105ab31775f21c2e0bd0a3deb9d2
kloglossi <kloglossi1995@gmail.com>
2019-07-29 13:34:53 +0000

    Creation du controller Operation avec les fonctions retrait ,depot,virement vide

---
+++ app/Http/Controllers/OperationController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OperationController extends Controller
+{
+    public function retrait(){
+
+    }
+
+    public function depot(){
+
+    }
+
+    public function virement(){
+
+    }
+}
